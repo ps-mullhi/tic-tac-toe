3983960a32d15fefd80fc3a7d9bd5547e1fccfe1
Puneet Singh <199150760+ps-mullhi@users.noreply.github.com>
2025-03-09 13:12:38 -0700

    Get user names and setup Players
    Ask each user for their name and require confirmation of each name.
    Then setup the player objects and return them.

--- main.rb
+++ main.rb
@@ -1,5 +1,50 @@
 require_relative 'lib/board.rb'
+require_relative 'lib/player.rb'
+require_relative 'lib/spot_value.rb'
 
-# debugger
-game_board = Board.new()
-game_board.display_board()
+def get_player_name()
+  name = ''
+  loop do
+    print('Enter your name: ')
+    name = gets.chomp
+    print("Confirm your name is #{name}? (Y/N): ")
+    confirmation = gets.chomp
+    break if confirmation == 'Y'
+  end
+  puts("#{name} confirmed.")
+  name
+end
+
+def clear_after_input_received(message)
+  print(message)
+  gets.chomp
+  puts(`clear`)
+end
+
+def setup_players()
+  player_one_name = get_player_name()
+  clear_after_input_received('Press anything to setup next player...')
+  player_two_name = get_player_name()
+  clear_after_input_received('Press anything to finish setup...')
+
+  player_one = Player.new(player_one_name, SpotValue::VALUE_X)
+  player_two = Player.new(player_two_name, SpotValue::VALUE_O)
+
+  return [player_one, player_two]
+end
+
+def main()
+  players = setup_players()
+  player_one = players[0]
+  player_two = players[1]
+
+  
+
+end
+
+main()
+
+
+
+
+
